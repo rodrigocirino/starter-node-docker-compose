55d2e1f88b253be9d9809d18485c160d11e7105c
Rodrigo Cirino <rodrigorcandrade@gmail.com>
2021-04-05 18:58:52 -0300

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -5,19 +5,28 @@ printf "\n........... Starting script%s ............\n" \ $0
 #
 #
 # Docker-compose + Dockerfile configuration
-# Mix local node_modules + remote nodeserver
-# --inspect-brk - brk means - stop before client code starting
+# Mix = local node_modules + remote nodeserver
+# Use --inspect-brk = brk means, stop before the client code starting
 #
 #
-# clean
+
+# clean old dependencies
 find . -name 'node_modules' -exec rm -rf {} \;
 find . -type f -name "package-lock.json" -exec rm -fv {} \; 
-#
-# install
+
+# install dependencies
 cd ./app/
 npm install
+
+# run eslint and prettier
 npm run lint
+
+# run jest with mocks
 npm t
+
+# up docker container
 cd ../config-dev/
-# docker-compose down
+# clean old containers
+docker-compose down
+# up container with specific yaml config
 docker-compose -f ./docker-compose-dev.yml up --build
